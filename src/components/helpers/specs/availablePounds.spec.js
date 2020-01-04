1998d7fc0d9e7071b4fa74596c5736fa4c9e65e6
Mat Greten <mgreten@gmail.com>
2020-01-04 11:56:35 -0700

    [bugfix] odd plate amounts and duplicate plates
    - We cannot use ood amounts of plates as we need to ensure both sides of the barbell are identical.
    - switching availablePounds to return half the amount of even plates in order to ensure that both sides of the barbell of have an even load. Before if we had 2 45's we'd essentially be telling each side of the barbell they had access to 2 45's. Meaning, we were calculating down the line based on having 4 45's. Doing at this level since we are already digging into available plates. Renaming to `availablePoundsPerSleeve` due to this change.

--- src/components/helpers/specs/availablePounds.spec.js
+++ src/components/helpers/specs/availablePounds.spec.js
@@ -1,10 +1,10 @@
-import availablePounds from "../availablePounds";
+import availablePoundsPerSleeve from "../availablePoundsPerSleeve";
 
-describe("availablePounds", () => {
+describe("availablePoundsPerSleeve", () => {
   describe("without any plate data", () => {
     test("returns empty array", () => {
       const plates = {};
-      expect(availablePounds(plates)).toEqual([]);
+      expect(availablePoundsPerSleeve(plates)).toEqual([]);
     });
   });
 
@@ -12,32 +12,48 @@ describe("availablePounds", () => {
     test("returns empty array", () => {
       const plates = {45: {weight: 45, available: 0}};
 
-      expect(availablePounds(plates)).toEqual([]);
+      expect(availablePoundsPerSleeve(plates)).toEqual([]);
     });
   });
 
-  test("returns array with 2 45s if there are 2 available 45s", () => {
-    const plates = {45: {weight: 45, available: 2}};
+  test("returns array with 2 45s if there are 4 available 45s", () => {
+    const plates = {45: {weight: 45, available: 4}};
 
-    expect(availablePounds(plates)).toEqual([45, 45]);
+    expect(availablePoundsPerSleeve(plates)).toEqual([45, 45]);
   });
 
   describe("with multiple plate weights", () => {
-    test("returns all available weights without unavailable weights", () => {
+    test("returns half of available weights without unavailable weights", () => {
       const plates = {
-        45: {weight: 45, available: 3},
+        45: {weight: 45, available: 4},
         35: {weight: 35, available: 0},
-        25: {weight: 25, available: 1},
+        25: {weight: 25, available: 2},
         10: {weight: 10, available: 2},
       };
 
-      const expectedResult = [45, 45, 45, 25, 10, 10];
-      const testResult = availablePounds(plates).sort();
+      const expectedResult = [45, 45, 25, 10 ];
+      const testResult = availablePoundsPerSleeve(plates).sort();
 
       // Order is not guaranteed and does not matter,
       // so we sort the results here to check equality.
       expect(testResult.sort()).toEqual(expectedResult.sort());
-    })
-  })
+    });
+
+    test("returns half of only the even number of available plates", () => {
+      //this ensures that we don't use a plate on one side and then not have a plate for the other side of the barbell
+      const plates = {
+        45: {weight: 45, available: 3},
+        25: {weight: 25, available: 1},
+        10: {weight: 10, available: 2},
+      };
+
+      const expectedResult = [45, 10];
+      const testResult = availablePoundsPerSleeve(plates).sort();
+
+      // Order is not guaranteed and does not matter,
+      // so we sort the results here to check equality.
+      expect(testResult.sort()).toEqual(expectedResult.sort());
+    });
+  });
 });
 
