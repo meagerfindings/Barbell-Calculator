4117311e0a1cb0462b3d239720ab35abdebdbd3d
Mat Greten <mgreten@gmail.com>
2020-01-04 12:45:20 -0700

    [feature] plate output and styling

--- src/components/weightDisplay/outPutContainer.jsx
+++ src/components/weightDisplay/outPutContainer.jsx
@@ -13,20 +13,31 @@ class OutPutContainer extends React.Component {
   };
 
   render() {
+    const plates =this.platesNeeded();
     return (
       <div className="output">
         Plates Needed:
-        {this.renderWorkSetWeight()}
+        {this.renderPlates(plates)}
       </div>
     )
   }
 
-  renderWorkSetWeight = () => {
+  platesNeeded = () => {
     const { workSetWeight, availablePlates } = this.props;
     if (_.isNaN(workSetWeight)) return null;
     if (workSetWeight < availablePlates.barWeight ) return null;
 
     return sleevePlates(workSetWeight, availablePlates);
+  };
+
+  renderPlates = plates => {
+    if (_.isNull(plates)) return null;
+
+    const listItems = plates.map((plate, index) =>
+      <li key={index}>{plate}</li>
+    );
+
+    return <ul className="plates-needed">{listItems}</ul>
   };
 }
 
